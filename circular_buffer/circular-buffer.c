d6d99e6ee2588aaab343158a257a089c1127e576
marpa <brundamarpadaga@gmail.com>
2026-04-02 13:33:10 -0700

    circular_buffer/circular-buffer.c:

--- circular_buffer/circular-buffer.c
+++ circular_buffer/circular-buffer.c
@@ -19,37 +19,39 @@ packet_t* create_packet(int id){
 
 void enqueue( circular_buffer_t *cb, packet_t *packet){
     if(cb->count == BUFFER_SIZE){
-        printf(" WARNING: Queue is full, discarding oldest data: %d at %lu \n" , cb->tail->sensor_value, cb->tail->timeStamp);
-        discardAtHead(cb); // delete the oldest data at head to make room for new data
+        printf(" WARNING: Queue is full, discarding oldest data: %d at %lu \n" , cb->head->id, cb->head->timeStamp);
+        discardAtHead(cb); // delete the oldest data at head to make room for new data and adjust the tail pointer
     
     }
+    
     cb->count++;
-     
-    if(cb->tail == cb->buffer + BUFFER_SIZE){ // if tail reaches the end of the buffer, wrap around to the start
-        cb->tail = cb->buffer;
-        *cb->tail = *packet;
-        return;
-    }
-    cb->tail+=1;
     *cb->tail = *packet;
+    cb->tail++; // move tail to the next packet
+    if(cb->tail >= cb->buffer + BUFFER_SIZE){
+        cb->tail = cb->buffer;
+    }
     free(packet);
 }
  
 //  tail -> 0 head -> 2 3 4 5 
-
+//dequeue returns the packet at the head
 packet_t* dequeue( circular_buffer_t *cb){
     if( cb->count == 0){
         printf(" WARNING: Cannot dequeue from empty queue! \n");
         return NULL;
     }
-    if(cb->head == cb->tail){
+    if(cb->head == cb->tail &&  cb->count == 1){
         packet_t* result = cb->head;
         cb->head = cb->tail = cb->buffer; // head and tail point to the start of the buffer
         cb->count = 0;
         return result;
     }
-    packet_t* result = cb->head;
-    cb->head += 1; // move head to the next packet
+    packet_t* result = (packet_t*) malloc(sizeof(packet_t));
+    *result = *cb->head; // copy the packet at the head
+    cb->head++; // move head to the next packet
+    if(cb->head >= cb->buffer + BUFFER_SIZE){
+        cb->head = cb->buffer;
+    }
     cb->count--;
     return result;
 }
@@ -59,42 +61,48 @@ void discardAtHead( circular_buffer_t *cb){
         printf(" WARNING: Cannot discard from empty queue! \n");
         return;
     }
-    if(cb->head == cb->tail){
-        cb->head = cb->tail = cb->buffer;
-        cb->count = 0;
-        return;
-    }
-    cb->head += 1;
     cb->count--;
+    cb->head++; // move head to the next packet
+    if(cb->head >= cb->buffer + BUFFER_SIZE){
+        cb->head = cb->buffer;
+    }
     return;
 }
 
 void print_buffer( circular_buffer_t *cb){
-    packet_t *current = cb->buffer;
+    packet_t *current = cb->head;
     printf("Buffer contents: \n");
     
     for(int i = 0; i < cb->count; i++){
         printf("Packet ID: %d, Sensor Value: %d, Timestamp: %lu \n", current->id, current->sensor_value, current->timeStamp);
         current++;
+        if(current >= cb->buffer + BUFFER_SIZE){
+            current = cb->buffer;
+        }
     }
+    printf("\n");
+    printf("Head: %d \n", cb->head->id);
+    printf("Tail: %d \n", cb->tail->id);
+    printf("Count: %d \n", cb->count);
+    printf("\n");
+
 }
 
 void delete_buffer(circular_buffer_t *cb){
-    packet_t* current = cb->head;
-    for(int i = 0; i < cb->count; i++){
-        packet_t* temp = current;
-        free(temp);
-        current++;
-    }
     free(cb);
-
 }
 
 int main(){
     circular_buffer_t *cb = new_circular_buffer();
-    for(int i = 0; i < 7; i++){
+    for(int i = 0; i < 10; i++){
         packet_t *packet = create_packet(i);
         enqueue(cb, packet);
+    }
+    print_buffer(cb);
+    for(int i = 0; i < 4; i++){
+        packet_t *packet = dequeue(cb);
+        printf("Dequeued packet : %d \n", packet->id);
+        free(packet);
     }
     print_buffer(cb);
     delete_buffer(cb);
