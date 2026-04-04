b99141b787c6e11c61e43167f5d2a1778d6b0c12
marpa <brundamarpadaga@gmail.com>
2026-04-03 23:04:12 -0700

    double LL

---
+++ doubleLL.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node_t;
+
+typedef struct LinkedList{
+    Node_t* head;
+}LinkedList_t;
+
+
+Node_t* createNode(int data){
+    Node_t* newNode = (Node_t*) malloc(sizeof(Node_t));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+LinkedList_t* createEmptyList(){
+    LinkedList_t* newList = (LinkedList_t*) malloc(sizeof(LinkedList_t));
+    newList->head = NULL;
+    return newList;
+}
+
+void addAtHead(LinkedList_t* list, Node_t* node){
+    if(list->head == NULL){
+        list->head = node;
+        return;
+    }
+    Node_t* head = list->head;
+    node->next = list->head;
+    head->prev = node;
+    list->head = node;
+
+
+}
+void printLinkedList(LinkedList_t* list){
+    Node_t* iter = list -> head;
+    printf("HEAD -> ");
+    while(iter!=NULL){
+        printf("%d -> ", iter->data);
+        iter = iter->next;
+    }
+    printf("\n");
+}
+
+void printReverse(LinkedList_t* list){
+    if(list->head == NULL){
+        return;
+    }
+    Node_t* tail = list->head;
+    
+    while(tail!=NULL && tail->next != NULL  ){
+        tail = tail -> next;
+    }
+    printf(" print reverse function %d \n", tail->data);
+    Node_t* iter = tail;
+    printf("TAIL -> ");
+    while(iter != NULL){
+        printf("%d -> ", iter->data);
+        iter = iter->prev;
+    }
+}
+
+
+void reverseLinkedList(LinkedList_t* list){
+
+    if(list->head == NULL){
+        return;
+    }
+
+    Node_t* iter = list->head;
+
+    while(iter->next!=NULL){
+        iter = iter->next;
+    }
+    // iter is the tail of the linkedList
+
+    //Node_t* tail = iter;
+    list->head = iter;
+    while(iter!=NULL){
+        Node_t* temp = iter->prev;
+        iter->next = temp;
+        iter->prev = temp->next->next;
+    }
+
+}
+
+
+int main(){
+    LinkedList_t* list1 = createEmptyList();
+    for(int i = 0; i<10; i++){
+        addAtHead(list1, createNode(i));
+    }
+    printLinkedList(list1);
+    printReverse(list1);
+    //reverseLinkedList(list1);
+    printLinkedList(list1);
+    return 0;
+}
