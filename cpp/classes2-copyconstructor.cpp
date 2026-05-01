827b1668e7e04c72ef27d548191d249d13728379
marpa <brundamarpadaga@gmail.com>
2026-05-01 12:44:19 -0700

    copy constructor and copy assignment operator

--- cpp/classes2-copyconstructor.cpp
+++ cpp/classes2-copyconstructor.cpp
@@ -30,13 +30,31 @@ class Array{
     // copy constructor
     Array(const Array &rhs){
         delete[] array;
-        array = new int[size];
         size = rhs.size;
+        array = new int[size];
         std::cout<< "copy constructor" << std::endl;
         for(int i=0; i<size;i++){
             array[i] = rhs.array[i];
         }
     }
+
+    // copy assignment operator
+    // object already exists and we are assigning a copy of another object to it
+    Array& operator=(const Array &rhs){
+        
+        if(this == &rhs){
+            return *this; // self assignment check
+        }
+        delete[] array;
+        size = rhs.size;
+        array = new int[size];
+        std::cout<< "copy assignment operator" << std::endl;
+        for(int i=0; i<size;i++){
+            array[i] = rhs.array[i];
+        }
+        return *this;
+    }
+
 
     ~Array(){
         delete[] array;
@@ -50,9 +68,14 @@ class Array{
 int main(){
     Array arr1(5); // Create an array of size 5
     arr1.printArray();
-    Array arr2(arr1); // No compiler error, but this will cause a shallow copy
+    Array arr2 = arr1; // No compiler error, but this will cause a shallow copy without a user defined copy constructor and thus arr1 and arr2 will point to the same memory location
     arr1.updateArray(1,9999);
+    arr1.printArray();
     arr2.printArray(); // change in array1 changed arr2 because arr2 basically is pointing to same memory location and is not its own individual object
-    // resulting in double freeing of same memory
+    // resulting in double freeing of same memory if copy constructor is not defined and thus causing a runtime error
+    Array arr3(10);
+    arr3.printArray();
+    arr3 = arr1; // copy assignment operator is called
+    arr3.printArray();
     return 0;
 }
