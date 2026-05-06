cc52c8c0f470b9fd037b39ede908ce31c7b86aa9
marpa <brundamarpadaga@gmail.com>
2026-05-06 12:55:25 -0700

    understanding damage done by passing objects by value

--- cpp/classes2-copyconstructor.cpp
+++ cpp/classes2-copyconstructor.cpp
@@ -29,10 +29,11 @@ class Array{
 
     // copy constructor
     Array(const Array &rhs){
-        delete[] array;
+        count++;
         size = rhs.size;
         array = new int[size];
         std::cout<< "copy constructor" << std::endl;
+        std::cout << "Object count: " << count << std::endl;
         for(int i=0; i<size;i++){
             array[i] = rhs.array[i];
         }
@@ -41,7 +42,7 @@ class Array{
     // copy assignment operator
     // object already exists and we are assigning a copy of another object to it
     Array& operator=(const Array &rhs){
-        
+        count++;
         if(this == &rhs){
             return *this; // self assignment check
         }
@@ -49,6 +50,7 @@ class Array{
         size = rhs.size;
         array = new int[size];
         std::cout<< "copy assignment operator" << std::endl;
+        std::cout << "Object count: " << count << std::endl; // to show that copy assignment operator is called and thus count is incremented
         for(int i=0; i<size;i++){
             array[i] = rhs.array[i];
         }
@@ -62,7 +64,15 @@ class Array{
     private:
     int* array;
     int size;
+    static int count ; // to keep track of number of objects created, includes all objects created through copy constructor and copy assignment operator, to show that they are being called when we expect them to be called
 };
+
+int Array::count = 0; // Definition of static member
+
+// BEFORE: pass by value to the function printArray, which will cause a copy of the array object to be created and thus copy constructor will be called, which is inefficient and unnecessary as we only want to print the array and not modify it
+void printArray(const Array& arr){ // passing by reference to avoid unnecessary copying of the array object, but still we can see that copy constructor is called when we pass arr1 by value to this function in the main function
+    arr.printArray();    
+}
 
 
 int main(){
@@ -71,11 +81,17 @@ int main(){
     Array arr2 = arr1; // No compiler error, but this will cause a shallow copy without a user defined copy constructor and thus arr1 and arr2 will point to the same memory location
     arr1.updateArray(1,9999);
     arr1.printArray();
-    arr2.printArray(); // change in array1 changed arr2 because arr2 basically is pointing to same memory location and is not its own individual object
+    arr2.printArray(); // BEFORE: change in array1 changed arr2 because arr2 basically is pointing to same memory location and is not its own individual object
     // resulting in double freeing of same memory if copy constructor is not defined and thus causing a runtime error
+    // AFTER: with copy constructor defined, arr2 is its own individual object and thus change in arr1 does not affect arr2
     Array arr3(10);
     arr3.printArray();
+    Array arr4 = arr3; // copy constructor is called
     arr3 = arr1; // copy assignment operator is called
     arr3.printArray();
+
+    for(int i = 0; i < 100; i++){
+        printArray(arr1); // copy constructor is called when passing arr1 by value to the function printArray
+    }
     return 0;
 }
