5ef28d001d786670df3681a27fa88cce674a9f2d
marpa <brundamarpadaga@gmail.com>
2026-05-21 13:29:39 -0700

    move constructor and move assignment operator

--- cpp/classes2-copyconstructor.cpp
+++ cpp/classes2-copyconstructor.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include <vector>
 
 class Array{
     
     public:
     // Constructor to initialize the array
-    Array(int size){
+    Array(std::string name, int size): size(size), name(name) { // Initialization list
         this->size = size;
+        this->name = name;
         array = new int[size];
         for(int i = 0; i < size; i++){
             array[i] = i;
@@ -13,6 +15,7 @@ class Array{
     }
 
     void printArray() const {
+        std::cout << "Array name: " << name << " (size: " << size << ")" << std::endl;
         for(int i = 0; i < size; i++){
             std::cout << array[i] << " ";
         }
@@ -30,6 +33,7 @@ class Array{
     // copy constructor
     Array(const Array &rhs){
         count++;
+        name = rhs.name + " (copy)";
         size = rhs.size;
         array = new int[size];
         std::cout<< "copy constructor" << std::endl;
@@ -42,12 +46,13 @@ class Array{
     // copy assignment operator
     // object already exists and we are assigning a copy of another object to it
     Array& operator=(const Array &rhs){
-        count++;
         if(this == &rhs){
             return *this; // self assignment check
         }
+        count++;
         delete[] array;
         size = rhs.size;
+        name = rhs.name + " (copy)";
         array = new int[size];
         std::cout<< "copy assignment operator" << std::endl;
         std::cout << "Object count: " << count << std::endl; // to show that copy assignment operator is called and thus count is incremented
@@ -57,13 +62,49 @@ class Array{
         return *this;
     }
 
+    // move constructor
+    Array(Array &&rhs) noexcept {
+        if(this != &rhs){
+            size = rhs.size;
+            name = rhs.name + " (move)";
+            array = rhs.array;
+            // Reset the source object
+            rhs.name = "";
+            rhs.size = 0;
+            rhs.array = nullptr;
 
+            std::cout<< "move constructor" << std::endl;
+            
+        }
+    }
+
+    // move assignment operator
+    Array& operator = (Array &&rhs) noexcept {
+        if(this != &rhs){
+            delete[] array;
+            size = rhs.size;
+            name = rhs.name + " (move assigned)";
+            array = rhs.array;
+
+            rhs.name = "";
+            rhs.size = 0;
+            rhs.array = nullptr;
+
+            std::cout<< "move assignment operator" << std::endl;
+            
+        }
+        return *this;
+    }
+
+    
+    // destructor
     ~Array(){
         delete[] array;
     }
     private:
     int* array;
     int size;
+    std::string name;
     static int count ; // to keep track of number of objects created, includes all objects created through copy constructor and copy assignment operator, to show that they are being called when we expect them to be called
 };
 
@@ -76,7 +117,7 @@ void printArray(const Array& arr){ // passing by reference to avoid unnecessary 
 
 
 int main(){
-    Array arr1(5); // Create an array of size 5
+    Array arr1("array1", 5); // Create an array of size 5
     arr1.printArray();
     Array arr2 = arr1; // No compiler error, but this will cause a shallow copy without a user defined copy constructor and thus arr1 and arr2 will point to the same memory location
     arr1.updateArray(1,9999);
@@ -84,14 +125,20 @@ int main(){
     arr2.printArray(); // BEFORE: change in array1 changed arr2 because arr2 basically is pointing to same memory location and is not its own individual object
     // resulting in double freeing of same memory if copy constructor is not defined and thus causing a runtime error
     // AFTER: with copy constructor defined, arr2 is its own individual object and thus change in arr1 does not affect arr2
-    Array arr3(10);
+    Array arr3("array3", 10);
     arr3.printArray();
     Array arr4 = arr3; // copy constructor is called
     arr3 = arr1; // copy assignment operator is called
     arr3.printArray();
 
-    for(int i = 0; i < 100; i++){
-        printArray(arr1); // copy constructor is called when passing arr1 by value to the function printArray
-    }
+    std::vector<Array> arrays;
+    arrays.push_back(std::move(arr1)); // move constructor is called
+    arrays.push_back(std::move(arr2));
+    arrays.push_back(std::move(arr3));
+    arrays.push_back(std::move(arr4)); // move constructor is called
+
+    
+
+    
     return 0;
 }
--- cpp/lvalue_rvalue.cpp
+++ cpp/lvalue_rvalue.cpp
@@ -18,10 +18,20 @@ int returnRvalue(){
 }
 int globalVar = 400; // global variable is an lvalue
 
+// Function that returns an lvalue reference
 int& returnLvalue(){
     return globalVar; // returns an lvalue, globalVar has locatable storage and an identifiable location in memory
 }
 
+// why print(int x) doesn't work
+// Because print(int x) takes the value by value, not by reference, so it creates a copy of the argument
+
+// lvalue reference: can bind to lvalues and is used to pass arguments by reference to avoid copying
+// rvalue reference: can bind to rvalues and is used to pass arguments by reference to avoid copying
+// const lvalue reference: can bind to both lvalues and rvalues, and is used to pass arguments by reference to avoid copying while ensuring the value cannot be modified
+// what is lvalue and rvalue in C++?
+// In C++, an lvalue (left value) is an expression that refers to a memory location and can appear on the left side of an assignment.
+// An rvalue (right value) is an expression that does not refer to a memory location and can only appear on the right side of an assignment.
 int main() {
     int a = 10; // 'a' is an lvalue
     print(a);   // Calls the lvalue reference version
@@ -29,8 +39,8 @@ int main() {
     print(20);  // Calls the rvalue reference version, generates a temporary rvalue with the value 20 
                 // Calls the const lvalue reference version, since it can bind to rvalues
     print(std::move(a)); // std::move converts 'a' to an rvalue, calls the rvalue reference version
-    
+                        // This allows the value to be moved rather than copied, which can be more efficient
     print(returnRvalue()); // Calls the rvalue reference version, returnRvalue() returns an rvalue
-    print(returnLvalue()); // Calls the lvalue reference version, returnLvalue() returns
+    print(returnLvalue()); // Calls the lvalue reference version, returnLvalue() returns an lvalue
     return 0;
 }
