71f5c1f209634362493f8227bdbd4a5cf115ff90
marpa <brundamarpadaga@gmail.com>
2026-08-12 12:36:37 -0700

    comparator exercise

--- cpp/scheduling.cpp
+++ cpp/scheduling.cpp
@@ -7,3 +7,75 @@ Input:
 "2,10,2"
 
 Output: 1 2 3 */
+
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<sstream>
+#include<algorithm>
+
+struct Job{
+    int job_id, start_time, duration;
+
+};
+
+std::vector<int> scheduler( std::vector<Job> jobs){
+
+    struct {
+        bool operator()(const Job& a, const Job& b){
+            if(a.start_time == b.start_time){
+                if(a.duration == b.duration){
+                    return a.job_id < b.job_id;
+                }
+                else{
+                    return a.duration < b.duration;
+                }
+            }
+            else{
+                return a.start_time < b.start_time;
+            }
+        }
+    } scheduleComp;
+
+    std::sort(jobs.begin(),jobs.end(), scheduleComp );
+
+    std::vector<int> result;
+    for( Job job : jobs){
+        result.push_back(job.job_id);
+    }
+
+    return result;
+    
+}
+
+int main() {
+    std::string line;
+    std::vector<Job> jobs;
+    std::string token;
+    
+    while (std::getline(std::cin, line)) {
+        
+        std::stringstream stream(line);
+        std::vector<std::string> tokens;
+
+        while(std::getline(stream, token,',')){
+            tokens.push_back(token);
+            // now tokens = {"3,10,5"}
+        }
+
+        Job job ;
+        job.job_id = std::stoi(tokens[0]);
+        job.start_time = std::stoi(tokens[1]);
+        job.duration = std::stoi(tokens[2]);
+
+        jobs.push_back(job);
+    }
+
+    std::vector<int> result = scheduler(jobs);
+
+    for ( int i : result){
+        std::cout << i << std::endl;
+    }
+    
+}
